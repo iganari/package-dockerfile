3341275c278ae7feff2e55864afd63f8ef0464d2
iganari <cigaguri@gmail.com>
2018-11-02 15:15:33 +0900

    add docker system command

--- xx_remove.sh
+++ xx_remove.sh
@@ -3,6 +3,21 @@
 set -xu
 set +e
 
-docker stop $(docker ps | awk 'NR>1 {print $1}')
-docker rm -f $(docker ps -a| awk 'NR>1 {print $1}')
-docker rmi -f $(docker images -q | uniq)
+force_hard_remove(){
+    docker stop $(docker ps | awk 'NR>1 {print $1}')
+    docker rm -f $(docker ps -a| awk 'NR>1 {print $1}')
+    docker rmi -f $(docker images -q | uniq)
+}
+
+force_soft_remove(){
+    docker system prune
+
+
+}
+
+# main
+force_hard_remove
+# force_soft_remove
+
+docker system df
+
