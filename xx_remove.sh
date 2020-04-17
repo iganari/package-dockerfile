6596a1414a5562dbf8631432408bafbe43d75838
iganari <cigaguri@gmail.com>
2020-04-18 00:41:09 +0900

    rename

--- xx_remove.sh
+++ xx_remove.sh
@@ -3,24 +3,6 @@
 set -xu
 set +e
 
-force_soft_remove() {
-    docker system prune -y
-}
-
-force_hard_remove() {
-    docker stop $(docker ps | awk 'NR>1 {print $1}')
-    docker rm -f $(docker ps -a| awk 'NR>1 {print $1}')
-    docker rmi -f $(docker images -q | uniq)
-}
-
-force_complete_remove() {
-    docker volume rm -f $(docker volume ls | awk 'NR>1 {print $2}')
-}
-
-
-# main
-force_hard_remove
-# force_soft_remove
-
-docker system df
-
+docker stop $(docker ps | awk 'NR>1 {print $1}')
+docker rm -f $(docker ps -a| awk 'NR>1 {print $1}')
+docker rmi -f $(docker images -q | uniq)
